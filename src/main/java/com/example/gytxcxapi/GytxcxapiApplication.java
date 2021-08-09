14ac9ffb03b1f85e72357058b5823dfb3d6589b8
LCL5 <122877453@qq.com>
2021-08-09 23:16:59 +0800

    Initial commit

---
+++ src/main/java/com/example/gytxcxapi/GytxcxapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.gytxcxapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GytxcxapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GytxcxapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gytxcxapi/GytxcxapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gytxcxapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GytxcxapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
